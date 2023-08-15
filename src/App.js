1978548e428c6f382da396152f13269a101b9fcd
Catarinals <132306244+Catarinals@users.noreply.github.com>
2023-08-15 19:52:26 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,26 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://catarina-portfolio-cls.netlify.app/"
+          rel="noreferrer"
+          target="_blank">
+          Catarina Santos
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://master--capable-churros-b3c915.netlify.app/"
+          rel="noreferrer"
+          target="_blank">
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
