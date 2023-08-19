40718ac661069da875ffd8b81dd3e20c0a160388
Catarinals <132306244+Catarinals@users.noreply.github.com>
2023-08-19 11:52:16 +0100

    Added search engine and correct icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -5,20 +5,35 @@ import Footer from "./Footer";
 import Weatherinfo from "./Weatherinfo";
 
 export default function Weather(props) {
+  const [city, setCity] = useState(props.defaultCity);
   const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
-    console.log(response);
+    //console.log(response);
     setWeatherData({
       ready: true,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl:
-        "https://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png",
+      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`,
       wind: response.data.wind.speed,
       city: response.data.name,
     });
+  }
+
+  function search() {
+    const apiKey = "1fd8093fa5ff12d796d7de756cc9d6b9";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric `;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
   }
 
   if (weatherData.ready) {
@@ -26,7 +41,7 @@ export default function Weather(props) {
       <div className="App">
         <div className="container">
           <div className="app border p-5 mt-5 rounded shadow">
-            <form id="search-form">
+            <form id="search-form" onSubmit={handleSubmit}>
               <div className="search-box">
                 <div className="Form">
                   <input
@@ -35,6 +50,7 @@ export default function Weather(props) {
                     id="enter-city"
                     autoComplete="off"
                     autoFocus="on"
+                    onChange={handleCityChange}
                   />{" "}
                   <button
                     type="submit"
@@ -58,10 +74,7 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    const apiKey = "1fd8093fa5ff12d796d7de756cc9d6b9";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric `;
-    axios.get(apiUrl).then(handleResponse);
-
+    search();
     return "Loading...";
   }
 }
