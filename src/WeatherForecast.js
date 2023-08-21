d4e8a18ed255a380ecafc9147c19216342b9e0d3
Catarinals <132306244+Catarinals@users.noreply.github.com>
2023-08-21 17:27:16 +0100

    I integrated the weather forecast API for the coordinates of a city

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,20 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(props);
+  }
+
+  let apiKey = "ea011et20fa37354b09b96e56b43o2f3";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
